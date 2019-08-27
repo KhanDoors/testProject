3ad33eb0fcd771fb7f564d4ac59d7bb13739ce4e
syyad khan <syyadkhan@yahoo.com>
2019-08-27 12:18:04 -0400

    back at it

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Pokecard from "./components/Pokecard";
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's go already!!</h1>
+      <Pokecard id={4} name="Chamander" type="Fire" exp={62} />
     </div>
   );
 }
